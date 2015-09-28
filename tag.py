683f6658596eec3a73b5ef5fa97c862b3f7db9ac
Brendan Whitfield <brendanw@windworksdesign.com>
2015-09-28 15:50:20 -0400

    using sets for option lists

--- tag.py
+++ tag.py
@@ -26,28 +26,45 @@ For issues and documentation: https://github.com/brendanwhitfield/tag-tool
 """
 
 
+
+
+def run(add_tags, remove_tags, files):
+    for f in files:
+        f = os.path.abspath(f)
+
+        # split out the dirs, the filename, and the extension
+        dirs, f   = os.path.split(f)
+        name, ext = os.path.splitext(f)
+
+        print(dirs)
+        print(name)
+        print(ext)
+
+
+
+
 def main():
     global verbose
     global root_dir
     global use_dirs
 
-    add_tags    = []
-    remove_tags = []
-    files       = []
+    add_tags    = set()
+    remove_tags = set()
+    files       = set()
 
-    for option in sys.argv:
+    for option in sys.argv[1:]:
         if option == "--help":
             print(help_text)
             return
         elif option == "--verbose":
             verbose = True
         else:
-            if option[1] == "+":
-                add_tags.append(option[1:]);
-            elif option[1] == "-":
-                remove_tags.append(option[1:]);
+            if option[0] == "+":
+                add_tags.add(option[1:]);
+            elif option[0] == "-":
+                remove_tags.add(option[1:]);
             elif os.path.isfile(option):
-                files.append(option)
+                files.add(option)
             else:
                 print("'%s' is not a valid file or command line option" % option)
 
@@ -58,6 +75,10 @@ def main():
     if (len(add_tags) + len(remove_tags)) == 0:
         print("please specify a tag operation")
         return
+
+    # run the tagger
+    run(add_tags, remove_tags, files)
+
 
 
 if(__name__ == "__main__"):
