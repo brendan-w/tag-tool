3b9330b9e925fa5581fba77c427776603ba9a71f
Brendan Whitfield <brendanw@windworksdesign.com>
2015-09-29 09:52:43 -0400

    crafted tag deletion regex

--- tag.py
+++ tag.py
@@ -3,12 +3,17 @@
 import os
 import re
 import sys
+from collections import namedtuple
 
+PathParts = namedtuple('PathParts', 'dirs name ext')
 
 # options
 verbose = True
 root_dir = ""
 use_dirs = False
+#tag_delims = "[ \\.,_-+&%%\\(\\)\\[\\]\\{\\}]"
+tag_delims = "[ \\.,_&%%\\-\\+\\(\\)\\[\\]\\{\\}]"
+
 
 help_text = """
 Usage:
@@ -25,6 +30,40 @@ Options:
 For issues and documentation: https://github.com/brendanwhitfield/tag-tool
 """
 
+# recursively finds the nearest .tagdir file denoting the limit for moving files
+def find_above(path, filename):
+
+    f = os.path.join(path, filename)
+
+    if os.path.isfile(f):
+        return path
+    else:
+        if path == "/":
+            return ""
+        else:
+            find_above(os.path.dirname(path), filename)
+
+
+def remove(tag, path_parts):
+
+    # remove tags from the name
+
+    # run as 3 different regexes for simplicity
+    front_pattern = "^" + tag + tag_delims
+    back_pattern  = tag_delims + tag + "$"
+    mid_pattern   = ("(?<=%s)" % tag_delims) + tag + tag_delims
+
+    print(path_parts.name)
+
+    n = re.sub(front_pattern, "", path_parts.name)
+    n = re.sub(back_pattern, "", n)
+    n = re.sub(mid_pattern, "", n)
+
+    print(n)
+
+    # remove tags from the dirs
+    if use_dirs:
+        pass
 
 
 
@@ -36,9 +75,11 @@ def run(add_tags, remove_tags, files):
         dirs, f   = os.path.split(f)
         name, ext = os.path.splitext(f)
 
-        print(dirs)
-        print(name)
-        print(ext)
+        path_parts = PathParts(dirs, name, ext)
+
+        for tag in remove_tags:
+            remove(tag, path_parts)
+
 
 
 
@@ -75,6 +116,9 @@ def main():
     if (len(add_tags) + len(remove_tags)) == 0:
         print("please specify a tag operation")
         return
+
+    root_dir = find_above(os.getcwd(), ".tagdir")
+    use_dirs = (root_dir != "") # could eventually be disabled by an option
 
     # run the tagger
     run(add_tags, remove_tags, files)
