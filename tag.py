29da39c349e9b3c690f7396d80a9fa4ab8aba332
Brendan Whitfield <brendanw@windworksdesign.com>
2015-10-04 00:43:03 -0400

    moved dir resolution to File class

--- tag.py
+++ tag.py
@@ -28,133 +28,13 @@ def test_options(root, name, dirs):
     settings.use_dirs = dirs
 
 
-# adds a tag to this file's name
-# if there is a dir to encode this tag, it will be removed from
-# the filename in the place() function
-def add(tag, path_parts):
-
-    # if the tag is already there, do nothing
-    if parts_have_tag(path_parts, tag):
-        return path_parts
-
-    if path_parts.name != "":
-        tag += settings.default_delim
-
-    return PathParts(path_parts.dirs,
-                     tag + path_parts.name,
-                     path_parts.ext)
-
-
-def remove(tag, path_parts):
-    name = path_parts.name
-    dirs = path_parts.dirs
-
-    # remove tags from the name
-    # Hard to combine these into one regex because python complains about not
-    # having fixed a length look-behind. Look-behind is necessary to prevent
-    # the leading delimeter from being eaten.
-
-    # WARNING: the order here is important. Deleting tags from the front or the
-    # back will cause inner tags to become front or back tags. This causes
-    # problems if there are two of the same tag adjacent to one-another.
-    mid_pattern   = ("(?<=%s)" % settings.tag_delims) + tag + settings.tag_delims
-    
-    #                (^|[ .,_-])tag($|[ .,_-])
-    edge_pattern  = "(^|" + settings.tag_delims + ")" + tag + "($|" + settings.tag_delims + ")"
-
-    # erase any tag instances from the name
-    if settings.use_name:
-        name = re.sub(mid_pattern, "", name)
-        name = re.sub(edge_pattern, "", name)
-
-    # remove tags from the dirs
-    if settings.use_dirs:
-        dirs = re.sub(mid_pattern, "", dirs)
-        dirs = re.sub(edge_pattern, "", dirs)
-
-    return PathParts(dirs, name, path_parts.ext)
-
-
-# recursive function that determines the filepath that encodes the most
-# of the given tagset.
-def find_best_path(path, tags):
-
-    best_path = path
-    best_tags_left = set(tags) # goal is to minimize len() for this
-
-    # search all of the directories at the current path
-    for d in dirs_at(os.path.join(settings.root_dir, path)):
-
-        d_tags = get_tags(d)
-
-        # if the tags of the directory name are all tags that we're looking for
-        if all([t in tags for t in d_tags]):
-            # we've found a valid dir to put the file in
-            
-            # prepare to recurse by removing the tags consumed by this dir name
-            next_tags = set(tags).difference(d_tags)
-
-            # recurse
-            r = find_best_path(os.path.join(path, d), next_tags)
-
-            # check to see if a better score was achieved
-            if(len(r[1]) < len(best_tags_left)):
-                best_path      = r[0]
-                best_tags_left = r[1]
-
-        # skip directories that contain tags we AREN'T looking for 
-
-    return (best_path, best_tags_left)
-
-
-# Only used if settings.use_dirs == True
-# Sinks a file back down the directory tree, according to its tags
-# Directories are favored as tag storage. Also handles deletion of tags
-# from dir names carrying multiple tags
-def resolve_dirs(path_parts):
-    tags = get_all_tags(path_parts)
-
-    # recurse to find the best directory path for this tagset
-    path, remaining_tags = find_best_path(settings.root_dir, tags)
-
-    # find out which tags were handled by directories
-    # and remove them from the filename
-    for tag in tags.difference(remaining_tags):
-        path_parts = remove(tag, path_parts)
-
-    return PathParts(path, path_parts.name, path_parts.ext)
-
-
-# parses tags for a single file, and returns a new filename
-def run_for_file(add_tags, remove_tags, f):
-    # return "/home/brendan/tag-tool/unknown.txt"
-    path_parts = f_split(f)
-
-    # remove the requested tags
-    for tag in remove_tags:
-        path_parts = remove(tag, path_parts)
-
-    current_tags = get_all_tags(path_parts)
-
-    # add the requested tags, if they're not already there
-    for tag in add_tags:
-        path_parts = add(tag, path_parts)
-
-    # reposition the file in the tree, favoring tags
-    # in the form of directory names
-    if settings.use_dirs:
-        path_parts = resolve_dirs(path_parts)
-
-    if path_parts.name == "":
-        path_parts = PathParts(path_parts.dirs, settings.no_tags_filename, path_parts.ext)
-
-    return f_join(path_parts)
-
 
 def run(add_tags, remove_tags, files):
-    for f in files:
-        print(f)
-        print(run_for_file(add_tags, remove_tags, f))
+    for filestr in files:
+        print(filestr)
+        f = File(filestr)
+        f.add_remove_tags(add_tags, remove_tags)
+        print(str(f))
 
 
 def main():
--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 
 import re
 import os
-from collections import namedtuple
 
 
 
@@ -50,38 +49,173 @@ General Utils
 """
 
 
-PathParts = namedtuple('PathParts', 'dirs name ext')
+class File:
+    def __init__(self, filestr):
+        # split out the dirs, the filename, and the extension
+        self.dirs, filestr  = os.path.split(filestr)
+        self.name, self.ext = os.path.splitext(filestr)
+
+        # if dirs are being used, do NOT consider the path
+        # to the root tag directory
+        if settings.use_dirs:
+            self.dirs = os.path.relpath(self.dirs, settings.root_dir)
+
+
+    def __str__(self):
+        path = self.dirs
+        filename = self.name + self.ext
+
+        # if a root dir was used, resolve the reference
+        if settings.use_dirs:
+            path = os.path.join(settings.root_dir, path)
+
+        return os.path.join(path, filename)
+
+
+    def get_tags(self):
+        """ returns a set of tags on this file """
+
+        tags = set()
+
+        if settings.use_name:
+            tags.update(get_tags(self.name))
+
+        if settings.use_dirs:
+            tags.update(get_tags(self.dirs))
+
+        return tags
+
+
+    def has_tag(self, tag):
+        if settings.use_name:
+            if has_tag(self.name, tag):
+                return True
+
+        if settings.use_dirs:
+            if has_tag(self.dirs, tag):
+                return True
+
+        return False
+
+
+    def add_remove_tags(self, add_tags, remove_tags):
+        # remove the requested tags
+        for tag in remove_tags:
+            self.__remove(tag)
+
+        # add the requested tags
+        for tag in add_tags:
+            self.__add(tag)
+
+        # reposition the file in the tree, favoring tags
+        # in the form of directory names
+        if settings.use_dirs:
+            self.__resolve_dirs()
+
+        if self.name == "":
+            self.name = settings.no_tags_filename
+
+
+    def __add(self, tag):
+        # adds a tag to this file's name
+        # if there is a dir to encode this tag, it will be removed from
+        # the filename in the resolve_dirs() function
+
+        # if the tag is already there, do nothing
+        if self.has_tag(tag):
+            return
+
+        if self.name != "":
+            tag += settings.default_delim
+
+        self.name = tag + self.name
+
+
+    def __remove(self, tag):
+        """ remove tags from the name """
+
+        # Hard to combine these into one regex because python complains about not
+        # having fixed a length look-behind. Look-behind is necessary to prevent
+        # the leading delimeter from being eaten.
+
+        # WARNING: the order here is important. Deleting tags from the front or the
+        # back will cause inner tags to become front or back tags. This causes
+        # problems if there are two of the same tag adjacent to one-another.
+        mid_pattern   = ("(?<=%s)" % settings.tag_delims) + tag + settings.tag_delims
+
+        #                (^|[ .,_-])tag($|[ .,_-])
+        edge_pattern  = "(^|" + settings.tag_delims + ")" + tag + "($|" + settings.tag_delims + ")"
+
+        # erase any tag instances from the name
+        if settings.use_name:
+            self.name = re.sub(mid_pattern, "", self.name)
+            self.name = re.sub(edge_pattern, "", self.name)
+
+        # remove tags from the dirs
+        if settings.use_dirs:
+            self.dirs = re.sub(mid_pattern, "", self.dirs)
+            self.dirs = re.sub(edge_pattern, "", self.dirs)
+
+
+
+    # Only used if settings.use_dirs == True
+    # Sinks a file back down the directory tree, according to its tags
+    # Directories are favored as tag storage. Also handles deletion of tags
+    # from dir names carrying multiple tags
+    def __resolve_dirs(self):
+        tags = self.get_tags()
+
+
+        # recurse to find the best directory path for this tagset
+        path, remaining_tags = find_best_path(settings.root_dir, tags)
+
+        # find out which tags were handled by directories
+        # and remove them from the filename
+        for tag in tags.difference(remaining_tags):
+            self.__remove(tag)
+
+        if settings.use_dirs:
+            self.dirs = os.path.relpath(path, settings.root_dir)
+
+
+
+
+
+# recursive function that determines the filepath that encodes the most
+# of the given tagset.
+def find_best_path(path, tags):
+
+    best_path = path
+    best_tags_left = set(tags) # goal is to minimize len() for this
+
+    # search all of the directories at the current path
+    for d in dirs_at(os.path.join(settings.root_dir, path)):
+
+        d_tags = get_tags(d)
+
+        # if the tags of the directory name are all tags that we're looking for
+        if all([t in tags for t in d_tags]):
+            # we've found a valid dir to put the file in
+            
+            # prepare to recurse by removing the tags consumed by this dir name
+            next_tags = set(tags).difference(d_tags)
+
+            # recurse
+            r = find_best_path(os.path.join(path, d), next_tags)
+
+            # check to see if a better score was achieved
+            if(len(r[1]) < len(best_tags_left)):
+                best_path      = r[0]
+                best_tags_left = r[1]
+
+        # skip directories that contain tags we AREN'T looking for 
+
+    return (best_path, best_tags_left)
 
 
 # lists only directories at the given path
 def dirs_at(path):
     return [ name for name in os.listdir(path) if os.path.isdir(os.path.join(path, name)) ]
-
-
-# splits an absolute file into an instance of PathParts
-def f_split(f):
-    # split out the dirs, the filename, and the extension
-    dirs, f   = os.path.split(f)
-    name, ext = os.path.splitext(f)
-
-    # if dirs are being used, do NOT consider the path
-    # to the root tag directory
-    if settings.use_dirs:
-        dirs = os.path.relpath(dirs, settings.root_dir)
-
-    return PathParts(dirs, name, ext)
-
-
-# reassemble a PathParts struct back into a filename
-def f_join(path_parts):
-    path = path_parts.dirs
-    filename = path_parts.name + path_parts.ext
-
-    # if a root dir was used, resolve the reference
-    if settings.use_dirs:
-        path = os.path.join(settings.root_dir, path)
-
-    return os.path.join(path, filename)
 
 
 def has_tag(s, tag):
@@ -93,19 +227,6 @@ def has_tag(s, tag):
     return re.search(pattern, s, flags) != None
 
 
-def parts_have_tag(path_parts, tag):
-
-    if settings.use_name:
-        if has_tag(path_parts.name, tag):
-            return True
-
-    if settings.use_dirs:
-        if has_tag(path_parts.dirs, tag):
-            return True
-
-    return False
-
-
 # returns the tagset for an arbitrary string
 def get_tags(s):
     tags = set(re.split(settings.tag_delims, s))
@@ -114,20 +235,6 @@ def get_tags(s):
         tags = set([x.lower() for x in tags])
 
     return set(filter(bool, tags)) # strain out empty strings
-
-
-# returns a set of tags on this file
-def get_all_tags(path_parts):
-
-    tags = set()
-
-    if settings.use_name:
-        tags.update(get_tags(path_parts.name))
-
-    if settings.use_dirs:
-        tags.update(get_tags(path_parts.dirs))
-
-    return tags
 
 
 def valid_tag(tag):
